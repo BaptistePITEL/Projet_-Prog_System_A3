827cc21d9d6d0e92c9b036f2d65c6013ec9c28a6
BaptistePITEL <45556246+BaptistePITEL@users.noreply.github.com>
2018-12-07 14:05:32 +0100

    Bug git

---
+++ Projet/Metier/Personnel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metier
+{
+    public abstract class Personnel
+    {
+        private string nom;
+
+        public Personnel(string nom)
+        {
+            this.nom = nom;
+        }
+
+        public abstract void log(string log);
+        public abstract void tick();
+    }
+}
